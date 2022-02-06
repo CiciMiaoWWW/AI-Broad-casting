ba51d97e9d805fb5fc5aeae408533ccd45cd9e2d
Liu Jikun <liujikun@hit.edu.cn>
2022-02-06 13:54:17 +0800

    Update len_mv_mp4.py

--- tools/len_mv_mp4.py
+++ tools/len_mv_mp4.py
@@ -3,6 +3,7 @@ from moviepy import *
 from moviepy.editor import *
 import numpy as np
 import imageio
+from tqdm import tqdm
 import pandas as pd
 class VideoGenerator:
     def __init__(self, width=1920, height=1080, step=300):
@@ -90,7 +91,7 @@ if __name__ == '__main__':
     pkl1 = '/mnt/bd/lwlq-workshop/Person_reID_baseline_pytorch/runs/detect/exp3/labels/飞书20220129-210653_labeled_smooth.pkl'
     pkl2 = '/mnt/bd/lwlq-workshop/Person_reID_baseline_pytorch/runs/detect/exp/labels/飞书20220129-210549_labeled_smooth.pkl'
     # 原始视频名称，注：下面list中第一个一定要放大全机位
-    src_list = ['飞书20220129-210659.mp4','飞书20220129-210653.mp4','飞书20220129-210549.mp4']
+    src_list = ['A30_rec.mp4','L30.mp4','R30.mp4']
     output_name = 'out.mp4'
     output_path = os.path.join(data_root, output_name)
     w = 1920
@@ -103,19 +104,19 @@ if __name__ == '__main__':
     crop_flag = True
     # 是否添加运镜：只有在裁剪模式下才可以运镜，运镜分zoom in / zoom out / shift left / shift right / shift up /shift down，运镜方式可组合
     len_mv_flag = True
-    # 可能的裁剪情况：大全（开场谢幕用）、每个人员的特写、舞台所有人全画幅展示（左右机位）、多个演员之间的互动
+    # 可能的裁剪情况：大全（开场谢幕用）、每个人员的特写、舞台所有人全画幅展示（左右机位）、多个演员之间的互动（后面再说）
     actor_amount =  3
     position_amount = 3
     assert position_amount==len(src_list), "错误提示：机位个数和输入视频数不等"
     # 开头和结尾slice帧数，初始化75，大约各3s大全，可改
     res_frame = 75
-    # 每个slice平均时长，假定每个slice的平均时长是6s（约150帧）
+    # 每个slice平均时长，假定每个slice的平均时长是6s（约150帧），可改
     avg_stay = 150
-    # 每个slice的最小时长，e.g., 4s
+    # 每个slice的最小时长，e.g., 4s，可改
     min_stay = 100
     # 演员id初始化 Tips: 更新底库需重写
     actor_id_list = [0,1,2]
-    # 检测框向外扩展系数
+    # 检测框向外扩展系数，可改
     padding_scale = 1.1
 
     ## 随机初始化
@@ -139,15 +140,15 @@ if __name__ == '__main__':
     # 机位id初始化
     position_id = slice_id.copy()
     for i in range(1,int(np.max(slice_id))+1):
-        position_id[position_id==i] = np.random.randint(1,len(src_list))
+        position_id[slice_id==i] = np.random.randint(1,len(src_list))
     position_id = position_id.astype(np.int32)
     print(np.unique(position_id))
     # 裁剪方式初始化
     # 可能的裁剪情况：每个人员的特写（演员id，0-2)、大全（开场谢幕用）4、舞台所有人全画幅展示（左右机位）3
-    crop_id = position_id.copy()
-    crop_id[crop_id==0] = len(actor_id_list)+2
+    crop_id = slice_id.copy()
+    crop_id[slice_id==0] = len(actor_id_list)+2
     for i in range(1,int(np.max(slice_id))+1):
-        crop_id[crop_id==i] = np.random.randint(0,len(actor_id_list)+1)
+        crop_id[slice_id==i] = np.random.randint(0,len(actor_id_list)+1)
     print(np.unique(crop_id))
 
     # 运镜方式初始化，可组合：zoom in / zoom out / shift left / shift right / shift up / shift down / 0 None
@@ -163,7 +164,7 @@ if __name__ == '__main__':
     ## 写MP4
     video_writer = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc(*'MP4V'), fps, (w, h))
     flag_start = True
-    for frame_cur_num in range(total_frame_num):
+    for frame_cur_num in tqdm(range(total_frame_num)):
         frame_cur_num = int(frame_cur_num)
         # 确定当前时刻是否换机位，如果不换则延续上一帧reader
         if flag_start:
@@ -180,7 +181,7 @@ if __name__ == '__main__':
         _,img=cap.read()  #read方法返回一个布尔值和一个视频帧。若帧读取成功，则返回True
 
         # 对img进行运镜、裁剪、resize 
-        # 如果需要单人特写：裁剪 + 去抖 + SR 
+        # 如果需要单人特写：裁剪 + 画质增强
         if crop_flag and crop_id[frame_cur_num]<=2:
             df = df_list[position_id[frame_cur_num]-1]
             info = df[(df['actor_id']==crop_id[frame_cur_num])&(df['frameid']==str(frame_cur_num+1))]
